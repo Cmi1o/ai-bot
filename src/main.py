e3525e0e2c8b36e894e29fdf93f0a27651101953
Georgiy Vasin <georgijvasin01@gmail.com>
2024-09-27 21:28:51 +0300

    include startup and shutdown in dispatcher

--- src/main.py
+++ src/main.py
@@ -7,11 +7,15 @@ from aiogram.exceptions import TelegramNetworkError
 
 from config import bot_token
 from app.gather_routers import router
+from app.dispatcher_builder import on_shutdown, on_startup
 
 
 async def main() -> None:
     bot = Bot(bot_token, parse_mode=ParseMode.MARKDOWN)
     dp = Dispatcher()
+    
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
     
     dp.include_router(router)
     
@@ -23,5 +27,6 @@ if __name__ == '__main__':
     
     try:
         asyncio.run(main())
-    except (KeyboardInterrupt, TelegramNetworkError):
-        print('Bot stopped')
+    except (KeyboardInterrupt, TelegramNetworkError) as error:
+        if isinstance(error, TelegramNetworkError):
+            print('Network error')
