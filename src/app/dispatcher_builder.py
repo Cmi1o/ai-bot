603fa9024cf2b6a087d5cf07697dae3a6be0698e
Georgiy Vasin <georgijvasin01@gmail.com>
2024-09-28 18:55:10 +0300

    create bot presentation in log class

--- src/app/dispatcher_builder.py
+++ src/app/dispatcher_builder.py
@@ -1,4 +1,16 @@
 from aiogram import Bot
+from aiogram.types import BotName
+
+
+class LogBot:
+    def __init__(self, bot: Bot) -> None:
+        self.bot = bot
+    
+    async def get_name(self) -> BotName:
+        return await self.bot.get_my_name()
+    
+    async def in_log(self) -> str:
+        return f'bot({await self.get_name()}, id={self.bot.id})'
 
 
 async def on_startup(bot: Bot) -> None:
