26101da0e201b96f87770a794b7f2caed5a3dbf8
Georgiy Vasin <georgijvasin01@gmail.com>
2024-11-28 22:23:57 +0300

    read and load redis url

--- src/config/loader.py
+++ src/config/loader.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     bot_token: SecretStr
     mistralai_token: SecretStr
+    redis_url: SecretStr
     
     model_config = SettingsConfigDict(
         env_file='.env',
--- src/config/reader.py
+++ src/config/reader.py
@@ -11,3 +11,4 @@ __all__ = (
 
 bot_token: const = settings.bot_token.get_secret_value()
 mistralai_token: const = settings.mistralai_token.get_secret_value()
+redis_url: const = settings.redis_url.get_secret_value()
